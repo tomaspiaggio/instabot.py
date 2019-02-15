c6d94e6cd04f1ce62a1c753608b3abfff531a7a3
Tomas Piaggio <tomas.piaggio12@gmail.com>
2019-02-15 09:33:59 -0300

    Added credential login with arguments

--- example.py
+++ example.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import sys
 
 from src import InstaBot
 from src.check_status import check_status
@@ -9,26 +10,33 @@ from src.feed_scanner import feed_scanner
 from src.follow_protocol import follow_protocol
 from src.unfollow_protocol import unfollow_protocol
 
+if len(sys.argv) <= 1:
+    raise ValueError('Credentials not passed as arguments')
+
+print('Logging in with credentials: ')
+print(f"Username: {sys.argv[1]}")
+print(f"Password: {sys.argv[2]}")
+
 bot = InstaBot(
-    login="username",  # Enter username (lowercase). Do not enter email!
-    password="password",
+    login=sys.argv[1],
+    password=sys.argv[2],
     like_per_day=1000,
     comments_per_day=0,
-    tag_list=["follow4follow", "f4f", "cute", "l:212999109"],
+    tag_list=["streetart", "art", "graffiti", "photography", "streetphotography", "street", "urbanart", "photooftheday", "instagood", "artist", "love", "bnw", "photo", "travel", "streetstyle", "urban", "artwork", "life", "city", "ig", "mural", "painting", "graffitiart", "blackandwhite", "fashion", "photographer", "portrait", "architecture", "picoftheday", "beach", "sunset", "nature", "sun", "sea", "sky", "pretty", "follow", "dog", "beautiful", "blue", "fun", "summer", "clouds", "beauty", "bestoftheday", "cool", "f", "hair", "amazing", "swag", "djiglobal", "gopro", "goproarg", "beachlife", "wanderlust", "escapecampervans", "chasingsunsets", "wanderers", "worldexplorer", "design", "interiordesign", "architecturephotography", "building", "landscape", "archilovers", "architect", "instatravel", "like", "arquitectura", "luxury", "home", "interior"],
     tag_blacklist=["rain", "thunderstorm"],
     user_blacklist={},
     max_like_for_one_tag=50,
-    follow_per_day=300,
-    follow_time=1 * 60 * 60,
-    unfollow_per_day=300,
+    follow_per_day=500,
+    follow_time=10 * 60 * 60,
+    unfollow_per_day=500,
     unlike_per_day=0,
     unfollow_recent_feed=True,  # If True, the bot will also unfollow people who dont follow you using the recent feed. Default: True
     time_till_unlike=3 * 24 * 60 * 60,  # 3 days
     unfollow_break_min=15,
     unfollow_break_max=30,
-    user_max_follow=0,
+    user_max_follow=9000,
     # session_file=False, # Set to False to disable persistent session, or specify custom session_file (ie ='myusername.session')
-    user_min_follow=0,
+    user_min_follow=100,
     log_mod=0,
     proxy="",
     # List of list of words, each of which will be used to generate comment
@@ -42,7 +50,6 @@ bot = InstaBot(
             "super",
             "good",
             "very good",
-            "good",
             "wow",
             "WOW",
             "cool",
@@ -80,7 +87,6 @@ bot = InstaBot(
         "food",
         "blog",
         "free",
-        "keren",
         "photo",
         "graphy",
         "indo",
@@ -124,7 +130,7 @@ bot = InstaBot(
         "_id",
         "bags",
     ],
-    unfollow_whitelist=["example_user_1", "example_user_2"],
+    unfollow_whitelist=['j_pou_g', 'tomaspiaggio', 'pilispadaro', 'additionstudios'],
     # Enable the following to schedule the bot. Uses 24H
     # end_at_h = 23, # Hour you want the bot to stop
     # end_at_m = 30, # Minute you want the bot stop, in this example 23:30
